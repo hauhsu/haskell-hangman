2688134ebc3c01231cf3db00a1fbdb6c7028b3f8
Hau Hsu <howard0617@gmail.com>
2018-04-29 22:41:42 +0800

    It works Q ! '

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Monad (forever)
 import Data.Char (toLower)
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, fromMaybe)
 import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
@@ -26,13 +26,17 @@ gameWords = do
     aw <- allWords
     return (filter gameLength aw)
     where gameLength w =
-        let l = length (w :: String)
-        in  minWordLength <= l && l < maxWordLength
+            let l = length (w :: String)
+            in     l >= minWordLength
+                && l < maxWordLength
 
 randomWord :: WordList -> IO String
 randomWord wl = do
     randomIndex <- randomRIO (0, length wl - 1)
     return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
 
 data Puzzle =
     Puzzle String [Maybe Char] [Char]
@@ -41,14 +45,88 @@ instance Show Puzzle where
     show (Puzzle _ discovered guessed) =
         (intersperse ' ' $
          fmap renderPuzzleChar discovered)
-        ++ " Guessed so far: " ++ guesed
+        ++ " Guessed so far: " ++ guessed
 
 freshPuzzle :: String -> Puzzle
-freshPuzzle = answer
+freshPuzzle answer =
     Puzzle answer nl []
-        where nl ans = map (const Nothing) ans
+        where nl = map (const Nothing) answer
 
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle answer _ _) c =
+    elem c answer
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guessed) c =
+    elem c guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar (Just c) = c
+renderPuzzleChar _ = '_'
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+    Puzzle word newFilledInSoFar (c : s) 
+    where zipper guessed wordChar guessChar =
+            if wordChar == guessed
+            then Just wordChar
+            else guessChar
+
+          newFilledInSoFar =
+              zipWith (zipper c) word filledInSoFar
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+    putStrLn $ "Your guess was: " ++ [guess]
+    case (charInWord puzzle guess, 
+        alreadyGuessed puzzle guess) of 
+        (_, True) -> do
+            putStrLn "You already guessed that\
+                \ character, pick\
+                \ something else!"
+            return puzzle
+        (True, _) -> do
+            putStrLn "This character was in the\
+                \ word, filling in the word\
+                \ accordingly."
+            return (fillInCharacter puzzle guess)
+        (False, _) -> do
+            putStrLn "This cahracter wasn't in\
+                \ the word, try again."
+            return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+    if (length guessed) > 7 then
+        do putStrLn "You lose!"
+           putStrLn $
+             "The word was: " ++ wordToGuess
+           exitSuccess
+        else return ()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+    if all isJust filledInSoFar then
+        do putStrLn "You win!"
+           exitSuccess
+    else return ()
+
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+    gameOver puzzle
+    gameWin puzzle
+    putStrLn $ "Current puzzle is: " ++ show puzzle
+    putStrLn "Guess a letter: "
+    guess <- getLine
+    case guess of
+        [c] -> handleGuess puzzle c >>= runGame
+        _   ->
+            putStrLn "Your guess must be a single character"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    word <- randomWord'
+    let puzzle =
+          freshPuzzle (fmap toLower word)
+    runGame puzzle
